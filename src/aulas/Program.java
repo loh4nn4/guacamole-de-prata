8122dff2a3664285c9a6d628739cb86624475b5a
Lohanna Silva <kamillys352@gmail.com>
2021-05-23 20:51:50 -0400

    Acrescentada mensagem de bom dia

--- src/aulas/Program.java
+++ src/aulas/Program.java
@@ -5,6 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		
 		System.out.println("Boa noite! Testando alterações pra commitar");
+		System.out.println("Bom dia!");
 	}
 
 }
